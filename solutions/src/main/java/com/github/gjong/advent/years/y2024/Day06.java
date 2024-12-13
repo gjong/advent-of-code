4fe2794171f7729a14df400f85f195277261703f
Gerben Jongerius <g.jongerius@jong-soft.com>
2024-12-13 09:16:23 +0100

    Refactor part2 to use multithreading for blockade simulation.
    Revised part2 to execute simulations in parallel using virtual threads, ensuring more efficient computation. Updated the logic with a new helper method `playRound` and adjusted the validation logic accordingly. Modified properties to reflect updated results for part2.

--- solutions/src/main/java/com/github/gjong/advent/years/y2024/Day06.java
+++ solutions/src/main/java/com/github/gjong/advent/years/y2024/Day06.java
@@ -10,6 +10,9 @@ import com.github.gjong.advent.geo.Point;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Day(year = 2024, day = 6, name = "Guard Gallivant")
 public class Day06 implements DaySolver {
@@ -72,13 +75,15 @@ public class Day06 implements DaySolver {
         validator.part1(visited.size());
     }
 
-    @Override
-    public void part2() {
+    private int playRound(List<Point> blockades) {
         var currentPos = startPoint;
         var currentDirection = new Point(0, -1);
 
         var visited = new HashSet<PointWithDirection>();
         while (gridBounds.inBounds(currentPos)) {
+            if (visited.contains(new PointWithDirection(currentPos, currentDirection))) {
+                return -1;
+            }
             visited.add(new PointWithDirection(currentPos, currentDirection));
 
             var updatedPos = currentPos.translate(currentDirection);
@@ -89,7 +94,35 @@ public class Day06 implements DaySolver {
             }
         }
 
-        validator.part2(visited.size());
+        return visited.size();
+    }
+
+    @Override
+    public void part2() {
+        var counter = new AtomicInteger();
+        try (var executors = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (var x = 0; x < gridBounds.width(); x++) {
+                for (var y = 0; y < gridBounds.height(); y++) {
+                    var point = new Point(x, y);
+                    if (!blockades.contains(point)) {
+                        var updatedBlockades = new ArrayList<>(blockades);
+                        updatedBlockades.add(point);
+
+                        executors.submit(() -> {
+                            if (playRound(updatedBlockades) == -1) {
+                                counter.incrementAndGet();
+                            }
+                        });
+                    }
+                }
+            }
+
+            executors.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        validator.part2(counter.get());
     }
 
     private record PointWithDirection(Point point, Point direction) {}
