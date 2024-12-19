39e9b7dabe8ff22669f7060ea750b7d1eb083218
Gerben Jongerius <g.jongerius@jong-soft.com>
2024-12-19 11:52:29 +0100

    Refactor Day16 part1 to use Dijkstra's for pathfinding
    Replaced custom pathfinding logic with a Dijkstra-based approach for efficiency and clarity. Enhanced the Step class to include cache and neighbor cost logic. Updated the input reading process to use CharGrid, simplifying grid handling.

--- solutions/src/main/java/com/github/gjong/advent/years/y2024/Day16.java
+++ solutions/src/main/java/com/github/gjong/advent/years/y2024/Day16.java
@@ -2,20 +2,21 @@ package com.github.gjong.advent.years.y2024;
 
 import com.github.gjong.advent.Day;
 import com.github.gjong.advent.DaySolver;
+import com.github.gjong.advent.algo.Node;
+import com.github.gjong.advent.algo.PathFinding;
+import com.github.gjong.advent.common.CharGrid;
 import com.github.gjong.advent.common.InputLoader;
 import com.github.gjong.advent.common.Validator;
+import com.github.gjong.advent.geo.Direction;
 import com.github.gjong.advent.geo.Point;
+import com.github.gjong.advent.geo.Vector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 @Day(year = 2024, day = 16, name = "Reindeer Maze")
 public class Day16 implements DaySolver {
-
-    private final Logger log = LoggerFactory.getLogger(Day16.class);
 
     private final InputLoader inputLoader;
     private final Validator validator;
@@ -25,61 +26,68 @@ public class Day16 implements DaySolver {
         this.validator = validator;
     }
 
-    record PathCost(Point previous, int cost) {}
-    record Path(Point previous, Point pos, Point direction, int cost) {
-        boolean sameDirection(Point other) {
-            return pos.translate(direction).equals(other);
-        }
+    private CharGrid grid;
+
+    @Override
+    public void readInput() {
+        grid = inputLoader.charGrid();
     }
 
     @Override
     public void part1() {
-        var grid = inputLoader.charGrid();
-        var startPos = grid.findChar('S').getFirst();
+        var startPos = new Step(grid.findChar('S').getFirst(), Point.east, new HashMap<>());
         var endPos = grid.findChar('E').getFirst();
 
-        // find paths
-        var visited = new HashMap<Point, PathCost>(); // point to cost mapping
-        var queue = new PriorityQueue<>(Comparator.comparingInt(Path::cost));
-        queue.add(new Path(null, startPos, Point.of(1, 0), 0));
-        while (!queue.isEmpty()) {
-            var currentPos = queue.poll();
-            visited.putIfAbsent(currentPos.pos(), new PathCost(currentPos.previous, currentPos.cost()));
+        startPos.setCost(0L);
+        var cost = PathFinding.dijkstra(
+                List.of(startPos),
+                step -> step.location.equals(endPos));
 
-            if (currentPos.pos().equals(endPos)) {
-                // found cheapest way out
-                break;
-            }
-
-            for (var neighbour : currentPos.pos().neighbours()) {
-                if (grid.at(neighbour) == '#' || visited.containsKey(neighbour)) {
-                    // skip walls, or already visited
-                    continue;
-                }
-
-                queue.add(new Path(
-                        currentPos.pos,
-                        neighbour,
-                        neighbour.subtract(currentPos.pos()),
-                        currentPos.cost() + (currentPos.sameDirection(neighbour) ? 1 : 1001)));
-            }
+        if (cost.isEmpty()) {
+            throw new IllegalStateException("No path found");
         }
 
-        if (log.isDebugEnabled()) {
-            var updating = visited.get(endPos).previous();
-            while (updating != null) {
-                grid.set(updating.x(), updating.y(), '0');
-                updating = visited.get(updating).previous();
-            }
-
-            log.debug("Grid after travel is:\n{}", grid.print());
-        }
-
-        validator.part1(visited.get(endPos).cost());
+        validator.part1(cost.get().cost());
     }
 
     @Override
     public void part2() {
 
+    }
+
+    private record StepDirection(Point location, Direction direction) {}
+
+    private class Step extends Node<Step> {
+        private final Point location;
+        private final Direction direction;
+        private final HashMap<StepDirection, Step> cache;
+
+        private Step(Point location, Direction direction, HashMap<StepDirection, Step> cache) {
+            this.location = location;
+            this.direction = direction;
+            this.cache = cache;
+        }
+
+        @Override
+        public Map<Step, Long> neighbours() {
+            var neighbours = new HashMap<Step, Long>();
+
+            location.neighbours()
+                    .stream()
+                    .filter(point -> grid.at(point) != '#')
+                    .forEach(step -> {
+                        var direction = Vector.direction(location, step);
+                        var cost = direction.equals(this.direction) ? 1L : 1001L;
+                        if (!cache.containsKey(new StepDirection(step, direction))) {
+                            var nextStep = new Step(step, direction, cache);
+                            cache.put(new StepDirection(step, direction), nextStep);
+                            neighbours.put(nextStep, cost);
+                        } else {
+                            neighbours.put(cache.get(new StepDirection(step, direction)), cost);
+                        }
+                    });
+
+            return neighbours;
+        }
     }
 }
